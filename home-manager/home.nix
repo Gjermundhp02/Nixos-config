24c1ab581f281c95ec5311850adead7708da9791
gjermundhp02 <gjermund.pedersen@gmail.com>
2024-08-25 16:57:24 +0200

    Initial commit

---
+++ home-manager/home.nix
@@ -0,0 +1,94 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  username,
+  hostname,
+  ...
+}: {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/home-manager):
+    # outputs.homeManagerModules.example
+
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  home = {
+    username = username;
+    homeDirectory = "/home/${username}";
+    packages = with pkgs; [
+      barrier
+      vscode
+      discord
+      obsidian
+      nodejs
+      prusa-slicer
+      gcc
+      libgcc
+      nodePackages.eas-cli
+    ];
+  };
+
+  programs = {
+    git = {
+      enable = true;
+      userName = "gjermundhp02";
+      userEmail = "gjermund.pedersen@gmail.com";
+      extraConfig = {
+        pull.rebase = false;
+      };
+    };
+    firefox = {
+      enable = true;
+      profiles.gjermund = {
+        settings = {
+          "full-screen-api.transition-duration.enter" = 0;
+          "full-screen-api.transition-duration.leave" = 0;
+          "full-screen-api.warning.timeout" = 0;
+          "signon.rememberSignons" = false;
+        };
+      };
+    };
+    ssh = {
+      enable = true;
+      matchBlocks = {
+          "skyhigh" = {
+              hostname = "10.212.170.43";
+              user = "ubuntu";
+          };
+          "server" = {
+              user = "gjermund";
+              hostname = "128.39.140.146";
+              port = 420;
+          };
+      };
+    };
+    bash = {
+      enable = true;
+      historyControl = ["ignoredups"];
+      shellAliases = {
+        ll = "ls -l";
+        la = "ls -la";
+        l = "ls";
+        rebuild = "sudo nixos-rebuild switch --flake ~/.config/home-manager/#";
+        buildtest = "sudo nixos-rebuild test --flake ~/.config/home-manager/#";
+        mkcd = "mkdir -p $1 && cd $1";
+      };
+    };
+  };
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  programs.home-manager.enable = true;
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,148 @@
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  username,
+  hostname,
+  ...
+}: {
+  # You can import other NixOS modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/nixos):
+    # outputs.nixosModules.example
+
+    # Or modules from other flakes (such as nixos-hardware):
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-ssd
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+
+    # Import your generated (nixos-generate-config) hardware configuration
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  nixpkgs = {
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      allowUnfree = true;
+      android_sdk.accept_license = true;
+    };
+  };
+
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = [ "nix-command" "flakes"];
+      # Opinionated: disable global registry
+      flake-registry = "";
+      # Workaround for https://github.com/NixOS/nix/issues/9574
+      nix-path = config.nix.nixPath;
+    };
+    # Opinionated: disable channels
+    channel.enable = false;
+
+    # Opinionated: make flake registry and nix path match flake inputs
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  };
+
+  environment.systemPackages = with pkgs; [
+    home-manager
+    networkmanager-openconnect
+  ];
+
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
+        enable = true;
+        setSocketVariable = true;
+    };
+  };
+
+  networking.hostName = hostname;
+  networking.networkmanager.enable = true;
+
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Oslo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "no";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "no";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Might not need this
+  services.openssh.enable = true;
+
+  users.users = {
+    ${username} = {
+      isNormalUser = true;
+      extraGroups = ["wheel" "networkmanager" "docker"];
+    };
+  };
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "24.05";
+}
