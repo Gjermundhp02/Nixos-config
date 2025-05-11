d62b7f48adf190041308f8fcd7c2677effb40aa6
gjermundhp02 <gjermund.pedersen@gmail.com>
2025-05-11 17:14:56 +0200

    Added krisp to discord

--- modules/home-manager/common.nix
+++ modules/home-manager/common.nix
@@ -9,7 +9,20 @@
   username,
   hostname,
   ...
-}: {
+}: let 
+  discordPatcher = pkgs.writers.writePython3Bin "discord-krisp-patcher" {
+    libraries = with pkgs.python3Packages; [
+      pyelftools
+      capstone
+    ];
+    flakeIgnore = [
+      "E265" # from nix-shell shebang
+      "E501" # line too long (82 > 79 characters)
+      "F403" # ‘from module import *’ used; unable to detect undefined names
+      "F405" # name may be undefined, or defined from star imports: module
+    ];
+  } (builtins.readFile ./discord-krisp-patcher.py);
+in {
   # You can import other home-manager modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
@@ -29,6 +42,9 @@
       gcc
       libgcc
     ];
+    activation.krispPatch = config.lib.dag.entryAfter ["writeBoundary"] ''
+      run ${pkgs.findutils}/bin/find -L ${config.home.homeDirectory}/.config/discord -name 'discord_krisp.node' -exec ${discordPatcher}/bin/discord-krisp-patcher {} \;
+    '';
   };
 
   dconf.settings = {
