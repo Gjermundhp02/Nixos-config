c1fda7563aaa1356996c5bc2d2f6075c2cd8dc62
gjermundhp02 <gjermund.pedersen@gmail.com>
2025-09-26 15:15:08 +0200

    refactor: add xlib libraries

--- shells/flake.nix
+++ shells/flake.nix
@@ -5,9 +5,13 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = import nixpkgs {
           inherit system;
           config = {
@@ -17,21 +21,21 @@
         };
         pinnedJDK = pkgs.jdk17;
         buildToolsVersion = "36.0.0";
-        ndkVersion = "26.1.10909125";
+        ndkVersion = "27.1.12297006";
         androidComposition = pkgs.androidenv.composeAndroidPackages {
           cmdLineToolsVersion = "19.0"; # CLI tools
           toolsVersion = "26.1.1"; # Legacy tools
           platformToolsVersion = "36.0.0"; # Platform tools
-          buildToolsVersions = [ buildToolsVersion "35.0.0" "34.0.0" ];
+          buildToolsVersions = [buildToolsVersion "35.0.0" "34.0.0"];
           includeEmulator = false;
           emulatorVersion = "30.3.4";
           # Target Android version
-          platformVersions = [ "35" ];
+          platformVersions = ["35"];
           includeSources = false;
           includeSystemImages = false;
-          systemImageTypes = [ "google_apis_playstore" ];
-          abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
-          cmakeVersions = [ "3.10.2" "3.22.1" ];
+          systemImageTypes = ["google_apis_playstore"];
+          abiVersions = ["armeabi-v7a" "arm64-v8a"];
+          cmakeVersions = ["3.10.2" "3.22.1"];
           includeNDK = true;
           ndkVersion = ndkVersion;
           useGoogleAPIs = false;
@@ -43,85 +47,95 @@
 
         sdk = androidComposition.androidsdk;
       in
-      with pkgs;
-      {
-        devShells = {
-          # Define the Android devShell
-          android = mkShell rec {
-            buildInputs = [
-              pinnedJDK
-              sdk
-              pkg-config
-              pkgs.nodePackages.eas-cli
-              pkgs.nodejs_22
-              pkgs.ffmpeg
-            ];
+        with pkgs; {
+          devShells = {
+            # Define the Android devShell
+            android = mkShell rec {
+              buildInputs = [
+                pinnedJDK
+                sdk
+                pkg-config
+                pkgs.nodePackages.eas-cli
+                pkgs.nodejs_22
+                pkgs.ffmpeg
+                pkgs.ungoogled-chromium
+              ];
 
-            JAVA_HOME = pinnedJDK;
-            ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
-            ANDROID_NDK_HOME = "${ANDROID_SDK_ROOT}/ndk/${ndkVersion}";
+              JAVA_HOME = pinnedJDK;
+              ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
+              ANDROID_NDK_HOME = "${ANDROID_SDK_ROOT}/ndk/${ndkVersion}";
 
-            # Made it so that ANDROID_NDK_HOME was ignored
-            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${buildToolsVersion}/aapt2";
+              # Made it so that ANDROID_NDK_HOME was ignored
+              GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${buildToolsVersion}/aapt2";
 
-            shellHook = ''
-              alias easd="eas build --local -p android --profile preview"
-              alias cda="cd .."
-              echo "Android development environment ready"
+              shellHook = ''
+                alias easd="eas build --local -p android --profile preview"
+                alias cda="cd .."
+                echo "Android development environment ready"
               '';
-          };
-          admin-api = pkgs.mkShell {
-            hardeningDisable = [ "fortify" ];
-            buildInputs = [
-              go
-              redis
-              gnumake
-              delve
-            ];
-          };
-          camControl = mkShell rec {
-            buildInputs = [
-              pinnedJDK
-              sdk
-              pkg-config
-              pkgs.nodePackages.eas-cli
-              pkgs.nodejs_22
-              pkgs.ffmpeg
-              pkgs.rustc
-              pkgs.cargo
-            ];
+            };
+            admin-api = pkgs.mkShell {
+              hardeningDisable = ["fortify"];
+              buildInputs = [
+                go
+                redis
+                gnumake
+                delve
+              ];
+            };
+            camControl = mkShell rec {
+              buildInputs = [
+                pinnedJDK
+                sdk
+                pkg-config
+                pkgs.nodePackages.eas-cli
+                pkgs.nodejs_22
+                pkgs.ffmpeg
+                pkgs.rustc
+                pkgs.cargo
+              ];
 
-            JAVA_HOME = pinnedJDK;
-            ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
-            ANDROID_NDK_HOME = "${ANDROID_SDK_ROOT}/ndk/${ndkVersion}";
+              JAVA_HOME = pinnedJDK;
+              ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
+              ANDROID_NDK_HOME = "${ANDROID_SDK_ROOT}/ndk/${ndkVersion}";
 
-            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+              RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 
-            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${buildToolsVersion}/aapt2";
+              GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${buildToolsVersion}/aapt2";
 
-            shellHook = ''
-              alias easd="eas build --local -p android --profile preview"
-              alias cda="cd .."
-              echo "Android development environment ready"
+              shellHook = ''
+                alias easd="eas build --local -p android --profile preview"
+                alias cda="cd .."
+                echo "Android development environment ready"
               '';
+            };
+            PROG2002 = mkShell {
+              buildInputs = with pkgs; [
+                git
+                gdb
+                libgcc
+                cmake
+                xorg.libX11
+                xorg.libXrandr
+                xorg.libXcursor
+                xorg.libXi
+                xorg.libXinerama
+                libGL
+                libGLU
+                glfw
+                glm
+                pkgs.python311Packages.glad
+              ];
+            };
+            IIKG3005 = mkShell {
+              buildInputs = with pkgs; [
+                git
+                opentofu
+                tofu-ls
+                openstackclient
+              ];
+            };
           };
-          PROG2002 = mkShell {
-            buildInputs = with pkgs; [
-              git
-              cmake
-              libGL
-              libGLU
-            ];
-          };
-          IIKG3005 = mkShell {
-            buildInputs = with pkgs; [
-              git
-              opentofu
-              tofu-ls
-              openstackclient
-            ];
-          };
-        };
-      }
+        }
     );
 }
